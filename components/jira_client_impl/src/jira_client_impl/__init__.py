23b99d90d9487a3b41f2875b8e1b21635c66e7fb
Shubham Tanwar <71093645+shubham739@users.noreply.github.com>
2026-03-26 10:57:07 -0400

    Openapi client library
    * remaining apis
    
    * openapi spec
    
    * added jira_service_api_client to gitignore + a make file to generate openapi spec in future _ some project.toml changes
    
    * remove jira_service_api_client from tracking
    
    * make file for generating jira_service_api_client
    
    * fixing issued
    
    ---------
    
    Co-authored-by: Subhradeep246 <subhradeep246@gmail.com>

--- components/jira_client_impl/src/jira_client_impl/__init__.py
+++ components/jira_client_impl/src/jira_client_impl/__init__.py
@@ -1,2 +1,3 @@
 """Public exports for the Jira client implementation package."""
+from jira_client_impl.jira_board import JiraBoard as JiraBoard
 from jira_client_impl.jira_impl import get_client as get_client
