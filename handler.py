11a4ddc9101b8bd187221e65b333efc26d434fab
Shubham Tanwar <shubhamtanwar72@gmail.com>
2026-04-03 09:19:52 -0400

    cleanup

--- handler.py
+++
@@ -1,7 +0,0 @@
-"""Adds mangum support for lambda deployment."""
-
-from mangum import Mangum
-
-from main import app
-
-handler = Mangum(app, lifespan="off", api_gateway_base_path="/default-deployment")
