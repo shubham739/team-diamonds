8dae86d9895b04468aaf03add211e64008b33802
Conor Zhang <cnrzhang@gmail.com>
2026-04-03 14:40:53 -0400

    missing main.py from root

--- components/jira_service/src/jira_service/handler.py
+++ components/jira_service/src/jira_service/handler.py
@@ -1,6 +1,6 @@
 """Adds mangum support for lambda deployment."""
 
-from main import app  # type: ignore[import-not-found]
 from mangum import Mangum
+from jira_service.main import app  # type: ignore[import-not-found]
 
 handler = Mangum(app, lifespan="off", api_gateway_base_path="/default-deployment")
