cfba1f6f56ad965d733d91362bfdc24ce7c22811
Conor Zhang <cnrzhang@gmail.com>
2026-04-03 14:01:49 -0400

    removed test_lambda_package, redid the deployment

--- test_lambda_package/handler.py
+++
@@ -1,6 +0,0 @@
-"""Adds mangum support for lambda deployment."""
-
-from main import app
-from mangum import Mangum
-
-handler = Mangum(app, lifespan="off")
--- test_lambda_package/work_mgmt_client_interface/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Public export surface for work_mgmt_client_interface."""
-
-from work_mgmt_client_interface.client import IssueTrackerClient as IssueTrackerClient
-from work_mgmt_client_interface.client import get_client as get_client
