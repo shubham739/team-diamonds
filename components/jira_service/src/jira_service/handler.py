07fc0c33a47583c1b2c43bbe9be88a895a0f65f9
Conor Zhang <cnrzhang@gmail.com>
2026-04-03 14:42:04 -0400

    ruff check missed again

--- components/jira_service/src/jira_service/handler.py
+++ components/jira_service/src/jira_service/handler.py
@@ -1,6 +1,7 @@
 """Adds mangum support for lambda deployment."""
 
 from mangum import Mangum
+
 from jira_service.main import app  # type: ignore[import-not-found]
 
 handler = Mangum(app, lifespan="off", api_gateway_base_path="/default-deployment")
